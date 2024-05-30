e3bc78b8afc4aec4c96aa1371f0e0ae1ba0083ef
Witsarut Supsi <witsarut.supsi@nectec.or.th>
2024-05-30 10:24:00 +0700

    Add Scene VK

--- static/VKBuild/TemplateData/responsive.js
+++ static/VKBuild/TemplateData/responsive.js
@@ -1,7 +1,7 @@
 (function(){
-    console.log('Responsive WebGL Template by SIMMER.io v2019.02.08');
-    console.log('Available at: https://assetstore.unity.com/packages/tools/gui/responsive-webgl-template-117308 for free!');
-    console.log('Host your WebGL Game at SIMMER.io for free!');
+    //console.log('Responsive WebGL Template by SIMMER.io v2019.02.08');
+    //console.log('Available at: https://assetstore.unity.com/packages/tools/gui/responsive-webgl-template-117308 for free!');
+    //console.log('Host your WebGL Game at SIMMER.io for free!');
 
     const q = (selector) => document.querySelector(selector);
 
@@ -67,7 +67,7 @@
             new MutationObserver(function (attributesMutation) {
                 this.disconnect();
                 setTimeout(setDimensions, 1)
-                q('.simmer').classList.add('hide');
+                //q('.simmer').classList.add('hide');
             }).observe(canvas, {attributes:true});
 
             this.disconnect();
